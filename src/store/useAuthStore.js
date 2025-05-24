75a64d84907776e0308428a87a6b7c36e184bed8
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-24 19:13:04 +0530

    feat: setup authStore using Zustand for authentication state management

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,56 @@
+import { create } from "zustand";
+import { auth, provider, signInWithPopup } from "@/utils/firebaseConfig";
+import { axiosInstance } from "@/utils/axios";
+import toast from "react-hot-toast";
+
+export default useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  signingIn: false,
+  loggingOut: false,
+  checkingAuth: true,
+
+  signup: async () => {
+    try {
+      set({ signingIn: true });
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const idToken = await user.getIdToken();
+      // signup the user in your backend
+      const response = await axiosInstance.post("auth/signup", { idToken });
+      set({ user: response.data.user, isAuthenticated: true });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Signup failed");
+    } finally {
+      set({ signingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      set({ loggingOut: true });
+      const response = await axiosInstance.get("/auth/logout");
+      set({ user: null, isAuthenticated: false });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Logout failed");
+    } finally {
+      set({ loggingOut: false });
+    }
+  },
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/checkAuth");
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+      });
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Authentication check failed"
+      );
+    } finally {
+      set({ checkingAuth: false });
+    }
+  },
+}));
