2ece6a4baee3aca1de05a405cdcfc10ce1e74a14
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-24 15:47:13 +0530

    feat: initial setup of user profile page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Layout from "./Layout/Layout";
 // pages import
 import Home from "./pages/Home/Home";
 import Idea from "./pages/Idea/Idea";
+import Profile from "./pages/Profile/Profile";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/idea/:id" element={<Idea />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
