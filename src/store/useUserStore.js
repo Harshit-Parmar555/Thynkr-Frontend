5975d76193170753307504a5de51c5262359ce6a
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-26 19:18:38 +0530

    feat: add fetchLoggedInUser function to useUserStore for fetching current user data

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -2,9 +2,27 @@ import { create } from "zustand";
 import { axiosInstance } from "@/utils/axios";
 import toast from "react-hot-toast";
 
+import { useAuthStore } from "./useAuthStore";
+
 export const useUserStore = create((set) => ({
   viewUser: null,
   fetchingUser: false,
+  fetchingLoggedInUser: false,
+
+  fetchLoggedInUser: async () => {
+    try {
+      set({ fetchingLoggedInUser: true });
+      const user = useAuthStore.getState().user;
+      const response = await axiosInstance.get(`/user/fetch-user/${user._id}`);
+      useAuthStore.setState({ user: response.data.user });
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Failed to fetch logged in user"
+      );
+    } finally {
+      set({ fetchingLoggedInUser: false });
+    }
+  },
 
   fetchUser: async (id) => {
     try {
